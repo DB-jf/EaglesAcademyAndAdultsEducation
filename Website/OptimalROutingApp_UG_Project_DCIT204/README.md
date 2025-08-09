5a21533126c0928afdc08b00d8d3c7d4e2e35705
BADIPEHTER DANIEL <bdaniel001@st.ug.edu.gh>
2025-08-09 10:20:52 +0000

    Delete Website directory
    Deleted - wrong main derectory created

--- Website/OptimalROutingApp_UG_Project_DCIT204/README.md
+++
@@ -1,2 +0,0 @@
-# OptimalROutingApp_UG_Project_DCIT204
-This is a group project repository for DCIT204-2025_IT-Students-Group16
--- Website/OptimalROutingApp_UG_Project_DCIT204/ugnav/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module se16 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-
-    opens se16 to javafx.fxml;
-    exports se16;
-}
